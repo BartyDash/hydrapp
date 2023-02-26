8b5651eb2170a7ae54116e7b7c22921f5e6b1a61
BartyDash <writetobartek@gmail.com>
2023-02-26 17:21:52 +0100

    save glass value to local storage by date

--- src/js/index.js
+++ src/js/index.js
@@ -11,17 +11,29 @@ console.log('HELLO 🚀')
 const glassCounterText = document.querySelector('.glass__counter--js');
 const buttonAdd = document.querySelector('.button-add--js');
 const buttonRemove = document.querySelector('.button-remove--js');
+const key = new Date().toISOString().slice(0, 10);
 
 let glassCounter = 0;
+
+const localStorageValue = localStorage.getItem(key);
+
+if(localStorageValue) {
+    glassCounter = localStorageValue;
+    glassCounterText.innerHTML = localStorageValue;
+} else {
+    localStorage.setItem(key, 0);
+}
 
 buttonAdd.addEventListener('click', () => {
     glassCounter++;
     glassCounterText.innerHTML = glassCounter;
+    localStorage.setItem(key, glassCounter);
 });
 
 buttonRemove.addEventListener('click', () => {
     if(glassCounter){
         glassCounter--;
         glassCounterText.innerHTML = glassCounter;
+        localStorage.setItem(key, glassCounter);
     }
 });
