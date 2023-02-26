46ade6de2470351b65d39737a49f49c4fbec4438
BartyDash <writetobartek@gmail.com>
2023-02-26 15:41:38 +0100

    water glass counter add and remove func

--- src/js/index.js
+++ src/js/index.js
@@ -7,3 +7,21 @@ import '../scss/main.scss';
 /* place your code below */
 
 console.log('HELLO 🚀')
+
+const glassCounterText = document.querySelector('.glass__counter--js');
+const buttonAdd = document.querySelector('.button-add--js');
+const buttonRemove = document.querySelector('.button-remove--js');
+
+let glassCounter = 0;
+
+buttonAdd.addEventListener('click', () => {
+    glassCounter++;
+    glassCounterText.innerHTML = glassCounter;
+});
+
+buttonRemove.addEventListener('click', () => {
+    if(glassCounter){
+        glassCounter--;
+        glassCounterText.innerHTML = glassCounter;
+    }
+});
