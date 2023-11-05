e0e6cdc0ad5097d80ff20b482bce843cabc4b4cf
BartyDash <writetobartek@gmail.com>
2023-11-06 00:47:00 +0100

    localstorage

--- src/main.js
+++ src/main.js
@@ -4,8 +4,15 @@ const counter = document.querySelector('.counter--js');
 
 let glasses = 0;
 
+const key = new Date().toISOString().slice(0, 10);
+
+if (localStorage.getItem(key)) {
+    glasses = parseInt(localStorage.getItem(key));
+}
+
 const setCounterValue = (value) => {
     counter.innerHTML = value;
+    localStorage.setItem(key, value);
 }
 
 setCounterValue(glasses);
